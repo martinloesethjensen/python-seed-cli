7660758510f134757bbb1625e733dcff860b791b
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-11-08 11:53:17 +0100

    :construction: Working on find and replace in more files in dir

--- application.py
+++ application.py
@@ -8,14 +8,25 @@ APP_BUILD_GRADLE = "/app/build.gradle"
 APP_NAME_CHANGE = "APP_NAME_CHANGE"
 KEYSTORE_PASSWORD = "KEYSTORE_PASSWORD"
 KEYSTORE_ALIAS = "KEYSTORE_ALIAS"
-PACKAGE_NAME = "dk.adaptmobile.android_seed"
+SEED_PACKAGE_NAME = "dk.adaptmobile.android_seed"
 APP_JAVA_PATH = "/app/src/main/java"
 
 
-def find_and_replace(new, old, file_name):
+def find_and_replace(find, replace, file_name):
     with fileinput.FileInput(file_name, inplace=True) as file:
         for line in file:
-            print(line.replace(old, new), end='')
+            print(line.replace(replace, find), end='')
+
+
+def find_replace_in_dir(directory, find, replace):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            with open(filepath) as file:
+                file_text = file.read()
+            file_text = file_text.replace(find, replace)
+            with open(filepath, "w") as file:
+                file.write(file_text)
 
 
 # This generates the keystore without the keytool cli.
@@ -30,16 +41,20 @@ def generate_keystore():
 def rename_package_dirs():
     os.chdir(root_module + APP_JAVA_PATH)
 
-    old_package_name_split = PACKAGE_NAME.split(".")
+    seed_package_name_split = SEED_PACKAGE_NAME.split(".")
     new_package_name_split = package_name.split(".")
 
-    for i in range(len(old_package_name_split)):
-        os.rename(old_package_name_split[i], new_package_name_split[i])
+    for i in range(len(seed_package_name_split)):
+        os.rename(seed_package_name_split[i], new_package_name_split[i])
         os.chdir(new_package_name_split[i])
+
+    os.chdir('../../../../../../..')
+    find_replace_in_dir(os.getcwd(), SEED_PACKAGE_NAME, package_name)
+    os.chdir('..')
 
 
 def rename_app_name():
-    find_and_replace(new=app_name, old=APP_NAME_CHANGE, file_name=root_module + APP_BUILD_GRADLE)
+    find_and_replace(find=app_name, replace=APP_NAME_CHANGE, file_name=root_module + APP_BUILD_GRADLE)
     print(f'Changed app_name to {app_name}')
 
 
@@ -49,8 +64,8 @@ def rename_root_module():
 
 
 def rename_keystore_fields():
-    find_and_replace(new=alias, old=KEYSTORE_ALIAS, file_name=root_module + APP_BUILD_GRADLE)
-    find_and_replace(new=password, old=KEYSTORE_PASSWORD, file_name=root_module + APP_BUILD_GRADLE)
+    find_and_replace(find=alias, replace=KEYSTORE_ALIAS, file_name=root_module + APP_BUILD_GRADLE)
+    find_and_replace(find=password, replace=KEYSTORE_PASSWORD, file_name=root_module + APP_BUILD_GRADLE)
 
 
 if __name__ == '__main__':
@@ -65,5 +80,5 @@ if __name__ == '__main__':
 
     alias = input("Alias for the keystore: ")
     password = input("Password for the keystore: ")
-    rename_keystore_fields()
-    generate_keystore()  # Should be run at the end
+    # rename_keystore_fields()
+    # generate_keystore()  # Should be run at the end
